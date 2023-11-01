77380b73951fe3a5f55a405ebf851ca3630e578b
Leo <volwahsenl@gmail.com>
2023-11-01 18:33:36 +0100

    icons from fontawesome work now properly

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 import App from './App.vue'
 import router from './router'
+import '@fortawesome/fontawesome-free/css/all.css'; 
+
+library.add(fas);
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,15 @@
 <script setup>
+
 </script>
 
 <template>
   <main>
     <h1>Homepage</h1>
+    <button class="btn btn-success">Press me</button>
+    <font-awesome-icon icon="coffee" />
   </main>
 </template>
 
 <style>
-
+  
 </style>
