cc635e46de8086141e768b4386390d806803f863
Leo <volwahsenl@gmail.com>
2023-11-02 15:04:41 +0100

    fontawesome works now showing icons

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from "./router/index.js"
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faHome);
+
 import 'bootstrap/dist/css/bootstrap.css';
 import bootstrap from "bootstrap/dist/js/bootstrap.bundle.js";
 
-createApp(App).use(router).use(bootstrap).mount('#app');
+createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).use(bootstrap).mount('#app');
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,15 @@
 <script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fas);
+
 export default {
-  name: 'HomeView'
+  name: 'HomeView',
+  components: {
+    FontAwesomeIcon,
+  },
 };
 </script>
 
@@ -8,7 +17,9 @@ export default {
   <div class="container">
     <row>
       <card title="Welcome to Vue.js with Bootstrap">
-        <font-awesome-icon icon="house" class="btn-success" />
+        <font-awesome-icon :icon="['fas', 'home']" class="btn-success"/>
+        <br>
+        <font-awesome-icon :icon="['fas', 'phone']" />
         <p>This is a sample component demonstrating how to integrate Bootstrap with Vue.js.</p>
         <button class="btn btn-success">Click me</button>
       </card>
